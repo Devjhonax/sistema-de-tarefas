485b6fa116d5d64cdc7fab1fb0b0672d8d624b3b
Thayane Gomes <thayaneleandrooo@gmail.com>
2026-01-28 20:38:27 -0300

    alteração

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 
 app.use('/', tarefaRoutes);
 
