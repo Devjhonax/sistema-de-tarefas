fda92a4f91b1d53f32f5b10a4bc5ccb07c91d34a
Thayane Gomes <thayaneleandrooo@gmail.com>
2026-01-27 23:30:44 -0300

    Corrigido app.js

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors({
-  origin: "https://seu-projeto.vercel.app"
-}));
+app.use(cors());
 
-app.use(tarefaRoutes);
+app.use('/', tarefaRoutes);
 
-sequelize.sync().then(() => {
-  console.log('Banco de dados sincronizado');
-  app.listen(process.env.PORT, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+sequelize.sync()
+  .then(() => {
+    console.log('Banco de dados sincronizado');
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Erro ao sincronizar o banco:', error);
   });
-}).catch((error) => {
-  console.error('Erro ao sincronizar o banco:', error);
-});
